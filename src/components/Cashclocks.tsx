1500dd8770af81dac2cfdf03f07b880c146b2d07
Don-Cryptus <don.cryptus@gmail.com>
2023-06-06 14:28:32 +0200

    tewst

--- src/components/Cashclocks.tsx
+++ src/components/Cashclocks.tsx
@@ -1,8 +1,8 @@
 import React, { Fragment } from "react";
 import { BsPlusCircle } from "react-icons/bs";
 
-import { ClockStore } from "../store/ClockStore";
 import { OverviewStore } from "../store/OverviewStore";
+import { ClockStore } from "../store/XlockStore";
 import Clock from "./Clock";
 
 const Cashclocks: React.FC = () => {
--- src/store/overviewStore.ts
+++ src/store/overviewStore.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
-import { ClockStore } from "./ClockStore";
+import { ClockStore } from "./XlockStore";
 
 type MoneyList = {
   id: string;
