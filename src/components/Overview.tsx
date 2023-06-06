0f95f73f55eac50448b6bb9b93519cdb2b14483e
Don-Cryptus <don.cryptus@gmail.com>
2023-06-06 14:28:45 +0200

    rename

--- src/components/Overview.tsx
+++ src/components/Overview.tsx
@@ -2,8 +2,8 @@ import React, { useEffect, useRef } from "react";
 import { AiOutlineClockCircle } from "react-icons/ai";
 import { BsCashCoin } from "react-icons/bs";
 import { FaBalanceScale } from "react-icons/fa";
+import { ClockStore } from "../store/ClockStore";
 import { OverviewStore } from "../store/OverviewStore";
-import { ClockStore } from "../store/XlockStore";
 import { timeString } from "../utils";
 
 const currencies = [
