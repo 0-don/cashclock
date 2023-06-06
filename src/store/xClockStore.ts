4bae52f441d1d3f680fc7dd34137dac0ad08debe
Don-Cryptus <don.cryptus@gmail.com>
2023-06-06 16:00:05 +0200

    clean up

--- src/store/xClockStore.ts
+++ src/store/xClockStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { moneyTime, randomId } from "../utils";
 import { OverviewStore } from "./xOverviewStore";
@@ -39,121 +40,129 @@ const initialState = () => ({
   moneyHour: "180",
 });
 
-export const ClockStore = create<ClockStore>()(
-  immer<ClockStore>((set, get) => ({
-    clockList: [initialState()],
-    startClock: (id) =>
-      set((state) => {
-        const { clockList } = state;
-        const i = clockList.findIndex((clock) => clock.id === id);
-        clockList[i].timerOn = true;
-        clockList[i].timerStart = Date.now() - clockList[i].timerTime;
-      }),
-    stopClock: (id) =>
-      set((state) => {
-        const { clockList } = state;
-        const i = clockList.findIndex((clock) => clock.id === id);
-        clockList[i].timerOn = false;
-      }),
-    deleteClock: (id) =>
-      set((state) => {
-        const i = state.clockList.findIndex((clock) => clock.id === id);
-        state.clockList.splice(i, 1);
-      }),
-    createClock: () =>
-      set((state) => {
-        state.clockList.push({
-          id: randomId(),
-          name: "",
-          timerOn: false,
-          timerStart: 0,
-          timerTime: 0,
-          money: 0,
-          moneyHour: "180",
-        });
-      }),
-    updateClock: (id) =>
-      set((state) => {
-        const { clockList } = state;
-        const i = clockList.findIndex((clock) => clock.id === id);
-        const money = moneyTime(clockList[i].timerTime, clockList[i].moneyHour);
-
-        clockList[i].money = money;
-        clockList[i].timerTime = Date.now() - clockList[i].timerStart;
-
-        OverviewStore.getState().updateMoneyList(clockList[i].id, money);
-      }),
-    updateClockTime: (id, time) => {
-      const clock = ClockStore.getState().clockList.find(
-        (clock) => clock.id === id
-      );
-      const clockTimerOn = clock?.timerOn ? true : false;
-
-      ClockStore.getState().stopClock(id);
-      OverviewStore.getState().stopOverviewClock();
-
-      set((state) => {
-        const { clockList } = state;
-        const i = clockList.findIndex((clock) => clock.id === id);
-
-        if (clockList[i].timerTime + time >= 0) {
-          clockList[i].timerTime += time;
-        } else {
-          clockList[i].timerTime = 0;
-        }
-
-        clockList[i].money = moneyTime(
-          clockList[i].timerTime,
-          clockList[i].moneyHour
-        );
-        OverviewStore.getState().updateMoneyList(id, clockList[i].money);
-      });
-      if (clockTimerOn) {
-        ClockStore.getState().startClock(id);
-        OverviewStore.getState().startOverviewClock();
-      }
-    },
-    resetClock: (id) =>
-      set((state) => {
-        const { clockList } = state;
-        const i = clockList.findIndex((clock) => clock.id === id);
-        clockList[i].money = 0;
-        clockList[i].timerTime = 0;
-        OverviewStore.getState().updateMoneyList(id, clockList[i].money);
-      }),
-    changeName: (id, name) =>
-      set((state) => {
-        const i = state.clockList.findIndex((clock) => clock.id === id);
-        state.clockList[i].name = name;
-      }),
-    changeMoneyHour: (id, moneyHour) =>
-      set((state) => {
-        const i = state.clockList.findIndex((clock) => clock.id === id);
-        state.clockList[i].moneyHour = moneyHour;
-      }),
-    stopResetClocks: () =>
-      set((state) => {
-        state.clockList.map((clock) => {
-          clock.timerOn = false;
-          clock.money = 0;
-          clock.timerTime = 0;
-          return clock;
-        });
-      }),
-    stopClocks: () =>
-      set((state) => {
-        state.clockList.map((clock) => {
-          clock.timerOn = false;
-          return clock;
-        });
-      }),
-    getHighestClock: () => {
-      const { clockList } = get();
-      return clockList.length === 0
-        ? initialState()
-        : clockList.reduce((prev, current) =>
-            prev.timerTime > current.timerTime ? prev : current
+export const ClockStore = create(
+  persist(
+    immer<ClockStore>((set, get) => ({
+      clockList: [initialState()],
+      startClock: (id) =>
+        set((state) => {
+          const { clockList } = state;
+          const i = clockList.findIndex((clock) => clock.id === id);
+          clockList[i].timerOn = true;
+          clockList[i].timerStart = Date.now() - clockList[i].timerTime;
+        }),
+      stopClock: (id) =>
+        set((state) => {
+          const { clockList } = state;
+          const i = clockList.findIndex((clock) => clock.id === id);
+          clockList[i].timerOn = false;
+        }),
+      deleteClock: (id) =>
+        set((state) => {
+          const i = state.clockList.findIndex((clock) => clock.id === id);
+          state.clockList.splice(i, 1);
+        }),
+      createClock: () =>
+        set((state) => {
+          state.clockList.push({
+            id: randomId(),
+            name: "",
+            timerOn: false,
+            timerStart: 0,
+            timerTime: 0,
+            money: 0,
+            moneyHour: "180",
+          });
+        }),
+      updateClock: (id) =>
+        set((state) => {
+          const { clockList } = state;
+          const i = clockList.findIndex((clock) => clock.id === id);
+          const money = moneyTime(
+            clockList[i].timerTime,
+            clockList[i].moneyHour
           );
-    },
-  }))
+
+          clockList[i].money = money;
+          clockList[i].timerTime = Date.now() - clockList[i].timerStart;
+
+          OverviewStore.getState().updateMoneyList(clockList[i].id, money);
+        }),
+      updateClockTime: (id, time) => {
+        const clock = ClockStore.getState().clockList.find(
+          (clock) => clock.id === id
+        );
+        const clockTimerOn = clock?.timerOn ? true : false;
+
+        ClockStore.getState().stopClock(id);
+        OverviewStore.getState().stopOverviewClock();
+
+        set((state) => {
+          const { clockList } = state;
+          const i = clockList.findIndex((clock) => clock.id === id);
+
+          if (clockList[i].timerTime + time >= 0) {
+            clockList[i].timerTime += time;
+          } else {
+            clockList[i].timerTime = 0;
+          }
+
+          clockList[i].money = moneyTime(
+            clockList[i].timerTime,
+            clockList[i].moneyHour
+          );
+          OverviewStore.getState().updateMoneyList(id, clockList[i].money);
+        });
+        if (clockTimerOn) {
+          ClockStore.getState().startClock(id);
+          OverviewStore.getState().startOverviewClock();
+        }
+      },
+      resetClock: (id) =>
+        set((state) => {
+          const { clockList } = state;
+          const i = clockList.findIndex((clock) => clock.id === id);
+          clockList[i].money = 0;
+          clockList[i].timerTime = 0;
+          OverviewStore.getState().updateMoneyList(id, clockList[i].money);
+        }),
+      changeName: (id, name) =>
+        set((state) => {
+          const i = state.clockList.findIndex((clock) => clock.id === id);
+          state.clockList[i].name = name;
+        }),
+      changeMoneyHour: (id, moneyHour) =>
+        set((state) => {
+          const i = state.clockList.findIndex((clock) => clock.id === id);
+          state.clockList[i].moneyHour = moneyHour;
+        }),
+      stopResetClocks: () =>
+        set((state) => {
+          state.clockList.map((clock) => {
+            clock.timerOn = false;
+            clock.money = 0;
+            clock.timerTime = 0;
+            return clock;
+          });
+        }),
+      stopClocks: () =>
+        set((state) => {
+          state.clockList.map((clock) => {
+            clock.timerOn = false;
+            return clock;
+          });
+        }),
+      getHighestClock: () => {
+        const { clockList } = get();
+        return clockList.length === 0
+          ? initialState()
+          : clockList.reduce((prev, current) =>
+              prev.timerTime > current.timerTime ? prev : current
+            );
+      },
+    })),
+    {
+      name: "clock-store",
+    }
+  )
 );
--- src/store/xOverviewStore.ts
+++ src/store/xOverviewStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { ClockStore } from "./xClockStore";
 
@@ -24,74 +25,79 @@ type OverviewStore = {
   resetStop: () => void;
 };
 
-export const OverviewStore = create<OverviewStore>()(
-  immer<OverviewStore>((set) => ({
-    timerOn: false,
-    timerStart: 0,
-    timerTime: 0,
-    money: "0.00",
-    moneyList: [],
-    currency: "EUR",
-    startOverviewClock: () =>
-      set((state) => {
-        if (!state.timerOn) {
-          state.timerOn = true;
-          state.timerStart = Date.now() - state.timerTime;
-        }
-      }),
-    stopOverviewClock: () =>
-      set((state) => {
-        const clockList = ClockStore.getState().clockList;
-        const activeClock = clockList.find((clock) => clock.timerOn === true);
-        if (!clockList.length || !activeClock) {
+export const OverviewStore = create(
+  persist(
+    immer<OverviewStore>((set) => ({
+      timerOn: false,
+      timerStart: 0,
+      timerTime: 0,
+      money: "0.00",
+      moneyList: [],
+      currency: "EUR",
+      startOverviewClock: () =>
+        set((state) => {
+          if (!state.timerOn) {
+            state.timerOn = true;
+            state.timerStart = Date.now() - state.timerTime;
+          }
+        }),
+      stopOverviewClock: () =>
+        set((state) => {
+          const clockList = ClockStore.getState().clockList;
+          const activeClock = clockList.find((clock) => clock.timerOn === true);
+          if (!clockList.length || !activeClock) {
+            state.timerOn = false;
+          }
+          state.money = state.moneyList
+            .reduce((prev, cur) => prev + cur.money, 0)
+            .toFixed(2);
+        }),
+      resetOverviewClock: () =>
+        set((state) => {
+          ClockStore.getState().stopClocks();
           state.timerOn = false;
-        }
-        state.money = state.moneyList
-          .reduce((prev, cur) => prev + cur.money, 0)
-          .toFixed(2);
-      }),
-    resetOverviewClock: () =>
-      set((state) => {
-        ClockStore.getState().stopClocks();
-        state.timerOn = false;
-        state.timerTime = 0;
-      }),
-    updateOverviewClock: () =>
-      set((state) => {
-        state.timerTime = Date.now() - state.timerStart;
-      }),
-    setCurrency: (name) =>
-      set((state) => {
-        state.currency = name;
-      }),
-    setMoney: (money = "0.00") =>
-      set((state) => {
-        ClockStore.getState().stopResetClocks();
-        state.money = money;
-        state.moneyList = [];
-      }),
-    updateMoneyList: (id, money) =>
-      set((state) => {
-        const { moneyList } = state;
-        const i = moneyList.findIndex((money) => money.id === id);
+          state.timerTime = 0;
+        }),
+      updateOverviewClock: () =>
+        set((state) => {
+          state.timerTime = Date.now() - state.timerStart;
+        }),
+      setCurrency: (name) =>
+        set((state) => {
+          state.currency = name;
+        }),
+      setMoney: (money = "0.00") =>
+        set((state) => {
+          ClockStore.getState().stopResetClocks();
+          state.money = money;
+          state.moneyList = [];
+        }),
+      updateMoneyList: (id, money) =>
+        set((state) => {
+          const { moneyList } = state;
+          const i = moneyList.findIndex((money) => money.id === id);
 
-        if (i < 0) {
-          moneyList.push({ id, money });
-        } else {
-          moneyList[i].money = money;
-        }
+          if (i < 0) {
+            moneyList.push({ id, money });
+          } else {
+            moneyList[i].money = money;
+          }
 
-        state.money = state.moneyList
-          .reduce((prev, cur) => prev + cur.money, 0)
-          .toFixed(2);
-      }),
-    resetStop: () =>
-      set((state) => {
-        ClockStore.getState().stopResetClocks();
-        state.money = "0.00";
-        state.moneyList = [];
-        state.timerOn = false;
-        state.timerTime = 0;
-      }),
-  }))
+          state.money = state.moneyList
+            .reduce((prev, cur) => prev + cur.money, 0)
+            .toFixed(2);
+        }),
+      resetStop: () =>
+        set((state) => {
+          ClockStore.getState().stopResetClocks();
+          state.money = "0.00";
+          state.moneyList = [];
+          state.timerOn = false;
+          state.timerTime = 0;
+        }),
+    })),
+    {
+      name: "overview-store",
+    }
+  )
 );
